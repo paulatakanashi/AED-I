d0a5ce6ee4eb2460b05096874e1c353a9e7423c1
paula <120468456+paulatakanashi@users.noreply.github.com>
2023-05-29 19:37:07 -0300

    Update and rename DASA to URI.2528.C

--- EP.10/URI.2528.C
+++ EP.10/URI.2528.C
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -15,6 +13,7 @@ int min(int a, int b) {
 
 void dfs(int current_city, int destination_city, int forbidden_city, int current_time, int *min_time) {
     if (current_city == destination_city) {
+        // Atualiza o menor tempo encontrado
         *min_time = min(*min_time, current_time);
         return;
     }
@@ -22,7 +21,9 @@ void dfs(int current_city, int destination_city, int forbidden_city, int current
     visited[current_city] = true;
     
     for (int i = 0; i < MAX_CITIES; i++) {
+        // Verifica se há uma rota entre as cidades e se a cidade não foi visitada e não é a cidade proibida
         if (graph[current_city][i] && !visited[i] && i != forbidden_city) {
+            // Realiza a chamada recursiva para a próxima cidade
             dfs(i, destination_city, forbidden_city, current_time + 1, min_time);
         }
     }
@@ -31,10 +32,10 @@ void dfs(int current_city, int destination_city, int forbidden_city, int current
 }
 
 int main() {
-    int n, m;
+    int num_cities, num_routes;
     
-    while (scanf("%d %d", &n, &m) != EOF) {
-        // Initialize graph and visited array
+    while (scanf("%d %d", &num_cities, &num_routes) != EOF) {
+        // Inicializa o grafo e o array de visitados
         for (int i = 0; i < MAX_CITIES; i++) {
             for (int j = 0; j < MAX_CITIES; j++) {
                 graph[i][j] = 0;
@@ -42,17 +43,19 @@ int main() {
             visited[i] = false;
         }
         
-        // Read routes
-        for (int i = 0; i < m; i++) {
-            int a, b;
-            scanf("%d %d", &a, &b);
-            graph[a][b] = graph[b][a] = 1;
+        // Lê as rotas
+        for (int i = 0; i < num_routes; i++) {
+            int city_a, city_b;
+            scanf("%d %d", &city_a, &city_b);
+            // Marca a rota entre as cidades no grafo
+            graph[city_a][city_b] = graph[city_b][city_a] = 1;
         }
         
         int curytiba, riacho, estadunido;
         scanf("%d %d %d", &curytiba, &riacho, &estadunido);
         
         int min_time = MAX_CITIES + 1;
+        // Chama a função DFS para encontrar o menor tempo necessário
         dfs(curytiba, riacho, estadunido, 0, &min_time);
         
         printf("%d\n", min_time);
