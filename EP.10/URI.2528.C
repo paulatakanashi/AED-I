730c5260a9c7d61da0c0a7ba52b7c8fbac72116f
paula <120468456+paulatakanashi@users.noreply.github.com>
2023-05-29 19:52:48 -0300

    Update URI.2528.C

--- EP.10/URI.2528.C
+++ EP.10/URI.2528.C
@@ -1,65 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
-#define MAX_CITIES 1000
-#define MAX_ROUTES (MAX_CITIES * (MAX_CITIES - 1) / 2)
+#define INF 0x3f3f3f3fU
+#define MAXSIZE 1001
 
-int graph[MAX_CITIES][MAX_CITIES];
-int visited[MAX_CITIES];
+typedef unsigned int uint;
 
-int min(int a, int b) {
-    return (a < b) ? a : b;
-}
+uint dist[MAXSIZE];
+uint graph[MAXSIZE][MAXSIZE];
 
-void dfs(int current_city, int destination_city, int forbidden_city, int current_time, int *min_time) {
-    if (current_city == destination_city) {
-        // Atualiza o menor tempo encontrado
-        *min_time = min(*min_time, current_time);
-        return;
-    }
-    
-    visited[current_city] = true;
-    
-    for (int i = 0; i < MAX_CITIES; i++) {
-        // Verifica se há uma rota entre as cidades e se a cidade não foi visitada e não é a cidade proibida
-        if (graph[current_city][i] && !visited[i] && i != forbidden_city) {
-            // Realiza a chamada recursiva para a próxima cidade
-            dfs(i, destination_city, forbidden_city, current_time + 1, min_time);
+void dijkstra(int, int, uint g[MAXSIZE][MAXSIZE], uint *);
+
+int main(){
+    int n, m, u, v;
+
+    while (scanf("%d %d", &n, &m) == 2){
+        memset(graph, INF, sizeof(graph));
+
+        for (int i = 0; i < m; ++i){
+            scanf("%d %d", &u, &v);
+            graph[u][v] = graph[v][u] = 1;
         }
-    }
-    
-    visited[current_city] = false;
-}
 
-int main() {
-    int num_cities, num_routes;
-    
-    while (scanf("%d %d", &num_cities, &num_routes) != EOF) {
-        // Inicializa o grafo e o array de visitados
-        for (int i = 0; i < MAX_CITIES; i++) {
-            for (int j = 0; j < MAX_CITIES; j++) {
-                graph[i][j] = 0;
-            }
-            visited[i] = false;
-        }
-        
-        // Lê as rotas
-        for (int i = 0; i < num_routes; i++) {
-            int city_a, city_b;
-            scanf("%d %d", &city_a, &city_b);
-            // Marca a rota entre as cidades no grafo
-            graph[city_a][city_b] = graph[city_b][city_a] = 1;
-        }
-        
-        int curytiba, riacho, estadunido;
-        scanf("%d %d %d", &curytiba, &riacho, &estadunido);
-        
-        int min_time = MAX_CITIES + 1;
-        // Chama a função DFS para encontrar o menor tempo necessário
-        dfs(curytiba, riacho, estadunido, 0, &min_time);
-        
-        printf("%d\n", min_time);
+        int c, r, e;
+        scanf("%d %d %d", &c, &r, &e);
+
+        for (int i = 0; i <= n; ++i)
+            graph[e][i] = graph[i][e] = INF;
+        dijkstra(c, n, graph, dist);
+        printf("%u\n", dist[r]);
     }
-    
     return 0;
+}
+
+void dijkstra(int s, int n, uint g[MAXSIZE][MAXSIZE], uint *d){
+    bool vis[MAXSIZE];
+    memset(vis, false, sizeof(vis));
+
+    memset(d, INF, (n + 1) * sizeof(uint));
+    d[s] = 0;
+
+    for (int i = 0; i <= n; ++i){
+        int v = -1;
+        for (int j = 0; j <= n; ++j){
+            if (!vis[j] && (v == -1 || d[j] < d[v]))
+                v = j;
+        }
+
+        if (d[v] == INF)
+            break;
+
+        vis[v] = true;
+        for (int j = 0; j <= n; ++j){
+            if (d[v] + g[v][j] < d[j])
+                d[j] = d[v] + g[v][j];
+        }
+    }
 }
